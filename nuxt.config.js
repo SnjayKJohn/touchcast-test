ec4a2540e1db5b8b1f6ffe1ec83b0a9f8d55ea56
SnjayKJohn <sanjaykjohnkalarikkal@gmail.com>
2022-05-15 03:20:53 +0530

    Vercel build issue fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors';
 export default {
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	ssr: false,
-	target: 'static',
-	fallback: true,
+
+	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
 		titleTemplate: '%s - touchcast-test',
 		title: 'touchcast-test',
